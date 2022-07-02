080dca6ecdb1eb76e2d700d41580cbbf2a79c274
IperENma <enmanuelhernandez1843@gmail.com>
2022-07-02 08:45:49 -0700

    test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -12,17 +12,7 @@ class test_file_storage(unittest.TestCase):
     """ class test file storage """
 
     def test_reload(self):
-        """ test reload from .json """
-        self.my_model = BaseModel()
-        self.my_model.save()
-        storage.reload()
-        objs = storage.all()
-        for obj_id in objs.keys():
-            obj_reload = objs[obj_id]
-        self.assertTrue(self.my_model.__dict__ == obj_reload.__dict__)
-
-    def test_newattr(self):
-        """ test add new attr """
+        """ test reload with new attr """
         self.my_model = BaseModel()
         self.my_model.name = "My_first_model"
         self.my_model.my_number = 89
