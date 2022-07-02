5ff80650af46aad7c6230d0732eba819eec03dba
IperENma <enmanuelhernandez1843@gmail.com>
2022-07-02 08:44:32 -0700

    test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -19,6 +19,16 @@ class test_file_storage(unittest.TestCase):
         objs = storage.all()
         for obj_id in objs.keys():
             obj_reload = objs[obj_id]
-        print(self.my_model)
-        print(obj_reload)
+        self.assertTrue(self.my_model.__dict__ == obj_reload.__dict__)
+
+    def test_newattr(self):
+        """ test add new attr """
+        self.my_model = BaseModel()
+        self.my_model.name = "My_first_model"
+        self.my_model.my_number = 89
+        self.my_model.save()
+        storage.reload()
+        objs = storage.all()
+        for obj_id in objs.keys():
+            obj_reload = objs[obj_id]
         self.assertTrue(self.my_model.__dict__ == obj_reload.__dict__)
