306dd1c420ab800b5aff6303d3d7c7c96e3fd565
IperENma <enmanuelhernandez1843@gmail.com>
2022-07-02 16:53:59 -0700

    tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -39,7 +39,3 @@ class test_file_storage(unittest.TestCase):
         self.assertTrue(self.my_model is not self.obj_reload)
         self.assertIsInstance(self.obj_reload, BaseModel)
         self.assertTrue(self.storage.all(), "My_first_model")
-
-    def test_reload(self):
-        """ method reload check """
-        self.assertIsNotNone(models.engine.file_storage.FileStorage().reload)
