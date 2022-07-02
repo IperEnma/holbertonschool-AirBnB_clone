f24b1d5ab12086c07a0e2d91aa604d181b07913d
IperENma <enmanuelhernandez1843@gmail.com>
2022-07-02 14:04:51 -0700

    push

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -48,3 +48,7 @@ class test_file_storage(unittest.TestCase):
     def test_reload(self):
         """ method reload check """
         self.assertIsNotNone(models.engine.file_storage.FileStorage().reload)
+
+    def test_models_all(self):
+        """ models storage all check """
+        self.assertIsNotNone(models.storage.all())
