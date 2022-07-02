669bbd2a4c32ed8d89541842a4f961efb0b0df04
IperENma <enmanuelhernandez1843@gmail.com>
2022-07-02 06:12:36 -0700

    pycode

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,20 +13,23 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 
+
 class FileStorage():
     """
     define class filestorage
     """
     __file_path = "file.json"
     __objects = {}
-    
+
     def all(self):
         """return objects"""
         return FileStorage.__objects
-    
+
     def new(self, obj):
         """sets in objects"""
-        FileStorage.__objects[str(obj.__class__.__name__) + '.' + str(obj.id)] = obj
+        name = str(obj.__class__.__name__)
+        _id = str(obj.id)
+        FileStorage.__objects[name + '.' + _id] = obj
 
     def save(self):
         """serializes __objects to the JSON file"""
@@ -42,7 +45,7 @@ class FileStorage():
             with open(FileStorage.__file_path, "r") as f:
                 obj = json.load(f)
             for key, value in obj.items():
-                 self.__objects[key] = eval(value["__class__"])(**value)
+                self.__objects[key] = eval(value["__class__"])(**value)
 
     def delete(self, key):
         """ delete objects"""
@@ -50,11 +53,18 @@ class FileStorage():
 
     @staticmethod
     def all_class():
-        return ["BaseModel", "User", "City", "Amenity", "Review", "Place", "State"]
+        return [
+                "BaseModel",
+                "User",
+                "City",
+                "Amenity",
+                "Review",
+                "Place",
+                "State"]
 
     @staticmethod
-    def set_atr(k , dic):
-        try:    
+    def set_atr(k, dic):
+        try:
             FileStorage.__objects[k].sett_atr(dic)
         except Exception:
             print("** no instance found **")
