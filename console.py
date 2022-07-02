ef8b07c563b20152e1789505f00c9d165180671f
IperENma <enmanuelhernandez1843@gmail.com>
2022-07-02 14:26:37 -0700

    test

--- console.py
+++ console.py
@@ -26,11 +26,11 @@ class HBNBCommand(cmd.Cmd):
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
-        return True
+        quit()
 
     def do_EOF(self, arg):
         """ Quit command to exit the program """
-        return True
+        quit()
 
     def precmd(self, args):
         """ before the command line line is interpreted """
