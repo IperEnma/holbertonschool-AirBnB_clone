0d0b4a53160e890f67bd6dc7ba14f8f32feb60f8
IperENma <enmanuelhernandez1843@gmail.com>
2022-07-02 17:12:40 -0700

    tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -25,6 +25,10 @@ class test_file_storage(unittest.TestCase):
         """checking doc class"""
         self.assertGreater(len(file_storage.FileStorage.__doc__), 1)
 
+    def test_instance(self):
+        """checking instance filestorage"""
+        self.assertIsNotNone(file_storage.FileStorage().all)
+
     def test_reload(self):
         """ test reload from json """
         self.my_model.name = "My_first_model"
